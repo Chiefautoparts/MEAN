03d99253f89248001760f155818f9f4cdacf8201
Chiefautoparts <chiefautoparts@outlook.com>
2017-09-08 13:26:45 -0700

    Node

---
+++ Node/LandingPage/app.js
@@ -0,0 +1,37 @@
+var http = require('http');
+
+var fs = require('fs');
+
+var server = http.createServer(function (request, response){
+	console.log('client request URL: ', request.url);
+
+	if(request.url === '/'){
+		fs.readFile('index.html', 'utf8', function(errors, contents){
+			response.writeHead(200, {'Content-Type': 'text/html'});
+			response.write(contents);
+			response.end();
+		});
+	}
+	else if (request.url === '/ninjas.html'){
+		fs.readFile('ninjas.html', 'utf8', function(errors, contents){
+			response.writeHead(200, {'Content-Type': 'text/html'});
+			response.write(contents);
+			response.end();
+		});
+	}
+	else if (request.url === '/dojo.html'){
+		fs.readFile('dojo.html', 'utf8', function(errors, contents){
+			response.writeHead(200, {'Content-Type': 'text/html'});
+			response.write(contents);
+			response.end();
+		});
+	}
+	else {
+		response.writeHead(404);
+		response.end("File not Found")	
+	}
+});
+
+server.listen(6789);
+
+console.log('Running on localhost:6789');
---
+++ Node/test.js
@@ -0,0 +1,1 @@
+console.log('L am running from node');
