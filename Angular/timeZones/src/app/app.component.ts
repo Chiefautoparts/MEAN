4bf508f7c1328e356172dd41b3c0cd23cc557a8b
Chiefautoparts <chiefautoparts@outlook.com>
2017-09-19 10:54:30 -0700

    timezones

--- Angular/timeZones/src/app/app.component.ts
+++ Angular/timeZones/src/app/app.component.ts
@@ -6,13 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
-  color = 'yellow';
+  curTime = new Date();
+  prevTime = null;
 
-  num = 1;
-  logNum(){
-  	console.log("num is ", this.num);
+  selectTimeZone(timezone){
+  	this.curTime = new Date();
+  	if (timezone === 'Mountain') {
+  		this.curTime.setTime(this.curTime.getHour() + 1);
+  	} else if (timezone === 'Central') {
+  		this.curTime.setTime(this.curTime.getHour() + 2 );
+  	} else if (timezone === 'Eastern') {
+  		this.curTime.setTime(this.curTime.getHour() + 3);
+  	}
+  	this.prevTime = timezone;
   }
 }
-
-
